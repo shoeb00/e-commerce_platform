0827bdb5a2f6819fcf75a162151c0b0306ac1d1c
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-14 02:09:51 +0530

    controller to delete a user account

--- controllers/user/profile.ts
+++ controllers/user/profile.ts
@@ -5,6 +5,7 @@ import {
     VUpdateProfile,
 } from "../../store/validators/user.validator";
 import { hashSync } from "bcrypt";
+import { objectIdValidator } from "../../store/validators";
 
 export const register = async (req: Request, res: Response) => {
     try {
@@ -45,6 +46,25 @@ export const updateUserDetails = async (req: Request, res: Response) => {
         );
         res.status(201).json({
             message: "User details updated successfully",
+            result,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const input = req.body.userId;
+        const { error } = objectIdValidator.validate(input);
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: "Invalid userId" });
+        }
+        const result = await database.User.deleteOne({ id: input });
+        res.status(201).json({
+            message: "User deleted successfully",
             result,
         });
     } catch (error) {
