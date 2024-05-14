45f48377e392a71d53f2840db9f5b5a53ab50b6d
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-14 09:19:49 +0530

    adding route and controller for authentication

---
+++ routes/authentication.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { authCallback, authenticationURL } from "../authentication";
+import { cache } from "../database/cache";
+const authRouter = Router();
+
+authRouter.get("/getAuthToken");
+
+authRouter.get("/google/callback", authCallback);
+
+authRouter.get("/login", (req, res) => {
+    res.status(200).json({ url: authenticationURL });
+});
+
+authRouter.get("/logout", async (req, res) => {
+    const token = req.headers.authorization || "";
+    await cache.delete(token);
+    res.redirect("/welcome");
+});
+
+export default authRouter;
