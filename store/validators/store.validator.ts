0107fa8af969f646477badf13f48c233afcbb386
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-15 16:45:29 +0530

    adding a validator for getProducts API

--- store/validators/store.validator.ts
+++ store/validators/store.validator.ts
@@ -7,13 +7,21 @@ import {
     objectIdValidator,
 } from "./index";
 
+export const VGetProduct = Joi.object({
+    name: Joi.string().min(3),
+    skip: Joi.number().min(0),
+    limit: Joi.number().positive(),
+    storeId: objectIdValidator.optional(),
+    category: categoryValidator.optional(),
+}).min(0);
+
 export const VAddProduct = Joi.object({
     name: nameValidator,
     category: categoryValidator,
     quantity: quantityValidator,
     price: priceValidator,
     storeId: objectIdValidator,
-}).min(4);
+}).min(5);
 
 export const VUpdateProductDetails = Joi.object({
     id: objectIdValidator.required(),
