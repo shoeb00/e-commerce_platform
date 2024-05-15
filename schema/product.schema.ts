c1cbf9dc4807c4be73dec9e68e8304b40f3cb742
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-15 16:45:10 +0530

    adding min limit for quantity in product schema

--- schema/product.schema.ts
+++ schema/product.schema.ts
@@ -14,6 +14,7 @@ const ProductSchema = new Schema(
         quantity: {
             type: Number,
             required: true,
+            min: 0,
         },
         category: {
             type: String,
