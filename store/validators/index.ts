ad1dbee531ff7ae005eeb4c0341ccadf820a5ce9
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-15 16:43:10 +0530

    making input params case insensitive

--- store/validators/index.ts
+++ store/validators/index.ts
@@ -3,21 +3,23 @@ import { EProductCategory } from "../enums/productCategory.enum";
 import { EOrderStatus } from "../enums/orderStatus.enum";
 import { ERoles } from "../enums/roles.enum";
 
-export const objectIdValidator = Joi.string().required().length(24);
+export const objectIdValidator = Joi.string()
+    .length(24)
+    .pattern(/^[0-9a-fA-F]{24}$/);
 export const quantityValidator = Joi.number().required().min(1).max(20);
 export const priceValidator = Joi.number().required().min(50);
 export const nameValidator = Joi.string().required().min(3).max(30);
 export const passwordValidator = Joi.string()
     .required()
     .min(5)
-    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"));
-export const categoryValidator = Joi.string().valid(
+    .pattern(/"^[a-zA-Z0-9]{3,30}$/);
+export const categoryValidator = Joi.string().insensitive().valid(
     ...Object.keys(EProductCategory)
 );
-export const orderStatusValidator = Joi.string().valid(
+export const orderStatusValidator = Joi.string().insensitive().valid(
     ...Object.keys(EOrderStatus)
 );
-export const roleValidator = Joi.string().valid(...Object.keys(ERoles));
+export const roleValidator = Joi.string().insensitive().valid(...Object.keys(ERoles));
 export const dateValidator = Joi.string()
     .required()
     .length(10)
@@ -25,4 +27,4 @@ export const dateValidator = Joi.string()
 export const phoneNumberValidator = Joi.string()
     .required()
     .length(10)
-    .pattern(new RegExp("^d{10}"));
+    .pattern(/"^d{10}/);
