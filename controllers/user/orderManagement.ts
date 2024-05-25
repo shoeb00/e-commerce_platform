8e2d91091ea459766e9b7d7b238211f37b577e2f
Syed Shoeb <syedshoeb886@gmail.com>
2024-05-25 17:00:56 +0530

    adding a controller to update order status

--- controllers/user/orderManagement.ts
+++ controllers/user/orderManagement.ts
@@ -8,11 +8,13 @@ import { IProduct } from "../../store/interfaces/product.interface";
 import { EOrderStatus } from "../../store/enums/orderStatus.enum";
 import { objectIdValidator } from "../../store/validators";
 import { IOrderProduct } from "../../store/interfaces/orderProducts.interface";
+import { VUpdateOrderStatus } from "../../store/validators/store.validator";
 
 export const placeOrder = async (req: Request, res: Response) => {
     try {
         const products: IOrderProduct[] = req.body;
         const productIds: string[] = [];
+        const storeId = req.params.storeId;
         if (!products.length) {
             return res.status(400).json({ message: "No product is added" });
         }
@@ -24,35 +26,46 @@ export const placeOrder = async (req: Request, res: Response) => {
             }
             productIds.push(value.productId);
         }
+        productIds.sort();
         const productDetails: IProduct[] = await database.Product.find({
             _id: { $in: productIds },
+            storeId,
         })
-            .sort({ id: 1 })
+            .sort({ _id: 1 })
             .lean();
-        products.sort((a: any, b: any) => a.productId - b.productId);
         const insufficientStock: IOrderProduct[] = [];
         const cart: IProduct[] = [];
         let totalPrice = 0;
-        for (let i = 0; i < products.length; i++) {
+        for (let i = 0, j = 0; i < products.length; i++, j++) {
+            if (products[i].productId !== productIds[j]) {
+                j++;
+                continue;
+            }
             const requestedQuantity = products[i].quantity;
-            const availableQuantity = productDetails[i].quantity;
-            if (availableQuantity > requestedQuantity) {
+            const availableQuantity = productDetails[j].quantity;
+            if (availableQuantity < requestedQuantity) {
                 insufficientStock.push(products[i]);
             } else {
-                const updatedQuantity = availableQuantity - requestedQuantity;
-                productDetails[i].quantity = updatedQuantity;
-                totalPrice += productDetails[i].price * requestedQuantity;
-                cart.push(productDetails[i]);
+                const stock = availableQuantity - requestedQuantity;
+                productDetails[j].stock = stock;
+                totalPrice += productDetails[j].price * requestedQuantity;
+                cart.push(productDetails[j]);
             }
+        }
+        if (!cart.length) {
+            return res.status(200).json({
+                message: "Requested products are not in stock",
+                insufficientStock,
+            });
         }
         const promises: Promise<object>[] = [];
         for (const product of cart) {
             const promise = database.Product.updateOne(
                 {
-                    id: product.id,
+                    _id: product._id?.toString(),
                 },
                 {
-                    quantity: product.quantity,
+                    quantity: product.stock,
                 }
             );
             promises.push(promise);
@@ -63,6 +76,7 @@ export const placeOrder = async (req: Request, res: Response) => {
             products,
             status: EOrderStatus.ORDER_CONFIRMED,
             userId: req.params.userId,
+            storeId,
         });
         return res.status(200).json({
             message: "Order place successfully",
@@ -96,14 +110,14 @@ export const cancelOrder = async (req: Request, res: Response) => {
         const promises: Promise<object>[] = [];
         for (const product of orderDetails.products) {
             const promise = database.Product.updateOne(
-                { id: product.productId },
+                { _id: product.productId },
                 { $inc: { quantity: product.quantity } }
             );
             promises.push(promise);
         }
         const result = await Promise.all(promises);
         const order = await database.Order.updateOne(
-            { id: orderId },
+            { _id: orderId },
             { status: EOrderStatus.CANCELLED }
         );
         return res
@@ -136,16 +150,44 @@ export const ordersHistory = async (req: Request, res: Response) => {
         const filter: any = {};
         if (status) filter.status = status;
         if (startDate || endDate) {
-            // filter.createdAt = {
-            //     $gte: new Date(startDate),
-            //     $lte: new Date(endDate),
-            // };
+            filter.createdAt = {
+                $gte: new Date(startDate),
+                $lte: new Date(endDate),
+            };
         }
         const orders = await database.Order.find(filter)
             .skip(skip)
             .limit(limit)
             .lean();
         return res.json({ message: "Success", orders });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+export const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+        const input = req.body;
+        const { error } = VUpdateOrderStatus.validate(input);
+        if (error) {
+            console.log(error.message);
+            return res.status(400).json({ message: error.message });
+        }
+        const result = await database.Order.findOneAndUpdate(
+            { storeId: req.params.userId, _id: input.id },
+            { status: input.status.toUpperCase() },
+            { new: true }
+        ).lean();
+        if (!result) {
+            return res
+                .status(400)
+                .json({ message: "No product found with this id" });
+        }
+        res.status(201).json({
+            message: "Product description updated successfully",
+            result,
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Something went wrong" });
